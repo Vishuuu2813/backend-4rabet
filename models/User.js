f3ce116374211ebbc0bf28fa82ace5235c384bbc
Vishwas Saxena <vvishwas221@gmail.com>
2025-04-23 16:13:12 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -24,8 +23,8 @@ const userSchema = new mongoose.Schema({
     required: true
   },
   timestamp: {
-    type: String, 
-    required: true
+    type: Date,
+    default: Date.now  // Automatically sets current date and time
   }
 });
 
